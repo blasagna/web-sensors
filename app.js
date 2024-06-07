a485882acddda15dd86223828b680049402edf54
Bob DiMaiolo <bdimaiolo@gmail.com>
2024-06-07 11:20:19 -0400

    add orientation and gravity and log to scollable readonly text area

--- app.js
+++ app.js
@@ -1,25 +1,23 @@
 'use strict'
 
 // todo:
-//  - get timestamps from Sensor interface too
-// - logs in scrolling text box
-// - button to clear logs
-// - replace UI logs with plot and data rate
-// - add gyroscope, orientation samples   
+// - periodically log latest sample and data rate
 // - add websocket client, send sensors to websocket server for signal processing
-
+// - plot samples vs. time
 
 const availableSensorsList = document.getElementById("availableSensors");
 const logElement = document.getElementById("log");
 
-var accelerometerSensor;
-var gravitySensor;
-var linearAccelerationSensor;
-var ambientLightSensor;
+let accelerometerSensor = null;
+let linearAccelerationSensor = null;
+let ambientLightSensor = null;
+let gyroscopeSensor = null;
+let orientationSensor = null;
 
 function log(msg) {
     console.log(msg);
-    logElement.innerHTML += `${msg}<br>`;
+    logElement.value += `${msg}\n`;
+    logElement.scrollTop = logElement.scrollHeight;
 }
 
 function checkPermission(sensorName) {
@@ -36,59 +34,151 @@ function checkPermission(sensorName) {
 }
 
 function startSensors() {
-    accelerometerSensor = new Accelerometer({ frequency: 60 });
-    accelerometerSensor.addEventListener("error", (error) => {
-        log(`Accelerometer error: ${error.name}`);
-    });
-    accelerometerSensor.addEventListener("activate", () => {
-        log("Accelerometer ready to measure");
-    });
-    accelerometerSensor.addEventListener("reading", () => {
-        log(`Acceleration along the Y-axis ${accelerometerSensor.y}`);
-        log(`Acceleration along the Z-axis ${accelerometerSensor.z}`);
-        log(`Acceleration along the X-axis ${accelerometerSensor.x}`);
-    });
-    accelerometerSensor.start();
+    if (window.Accelerometer) {
+        try {
+            accelerometerSensor = new Accelerometer();
+            accelerometerSensor.addEventListener("error", (error) => {
+                log(`Accelerometer error: ${error.name}`);
+            });
+            accelerometerSensor.addEventListener("activate", () => {
+                log("Accelerometer ready to measure");
+            });
+            accelerometerSensor.addEventListener("reading", () => {
+                const t = accelerometerSensor.timestamp;
+                log(`Acceleration along the Y-axis ${accelerometerSensor.y}, time ${t}`);
+                log(`Acceleration along the Z-axis ${accelerometerSensor.z}, time ${t}`);
+                log(`Acceleration along the X-axis ${accelerometerSensor.x}, time ${t}`);
+            });
+            accelerometerSensor.start();
+        } catch (error) {
+            // Handle construction errors.
+            if (error.name === "SecurityError") {
+                // See the note above about permissions policy.
+                log("Accelerometer sensor construction was blocked by a permissions policy.");
+            } else if (error.name === "ReferenceError") {
+                log("Accelerometer sensor is not supported by the User Agent.");
+            } else {
+                throw error;
+            }
+        }
+    }
 
-    gravitySensor = new GravitySensor({ frequency: 60 });
-    gravitySensor.addEventListener("error", (error) => {
-        log(`Gravity error: ${error.name}`);
-    });
-    gravitySensor.addEventListener("activate", () => {
-        log("Gravity ready to measure");
-    });
-    gravitySensor.addEventListener("reading", () => {
-        log(`Gravity along the X-axis ${gravitySensor.x}`);
-        log(`Gravity along the Y-axis ${gravitySensor.y}`);
-        log(`Gravity along the Z-axis ${gravitySensor.z}`);
-    });
-    gravitySensor.start();
+    if (window.LinearAccelerationSensor) {
+        try {
+            linearAccelerationSensor = new LinearAccelerationSensor();
+            linearAccelerationSensor.addEventListener("error", (error) => {
+                log(`Linear acceleration error: ${error.name}`);
+            });
+            linearAccelerationSensor.addEventListener("activate", () => {
+                log("Linear acceleration ready to measure");
+            });
+            linearAccelerationSensor.addEventListener("reading", () => {
+                const t = linearAccelerationSensor.timestamp;
+                log(`Linear acceleration along the X-axis ${linearAccelerationSensor.x}, time ${t}`);
+                log(`Linear acceleration along the Y-axis ${linearAccelerationSensor.y}, time ${t}`);
+                log(`Linear acceleration along the Z-axis ${linearAccelerationSensor.z}, time ${t}`);
+            });
+            linearAccelerationSensor.start();
+        } catch (error) {
+            // Handle construction errors.
+            if (error.name === "SecurityError") {
+                // See the note above about permissions policy.
+                log("Linear acceleration sensor construction was blocked by a permissions policy.");
+            } else if (error.name === "ReferenceError") {
+                log("Linear acceleration sensor is not supported by the User Agent.");
+            } else {
+                throw error;
+            }
+        }
+    }
 
-    linearAccelerationSensor = new LinearAccelerationSensor({ frequency: 60 });
-    linearAccelerationSensor.addEventListener("error", (error) => {
-        log(`Linear acceleration error: ${error.name}`);
-    });
-    linearAccelerationSensor.addEventListener("activate", () => {
-        log("Linear acceleration ready to measure");
-    });
-    linearAccelerationSensor.addEventListener("reading", () => {
-        log(`Linear acceleration along the X-axis ${linearAccelerationSensor.x}`);
-        log(`Linear acceleration along the Y-axis ${linearAccelerationSensor.y}`);
-        log(`Linear acceleration along the Z-axis ${linearAccelerationSensor.z}`);
-    });
-    linearAccelerationSensor.start();
+    if (window.AmbientLightSensor) {
+        try {
+            ambientLightSensor = new AmbientLightSensor();
+            ambientLightSensor.addEventListener("error", (error) => {
+                log(`Ambient light sensor error: ${error.name}`);
+            });
+            ambientLightSensor.addEventListener("activate", () => {
+                log("Ambient light ready to measure");
+            });
+            ambientLightSensor.addEventListener("reading", () => {
+                const t = ambientLightSensor.timestamp;
+                log(`Current light level: ${ambientLightSensor.illuminance}, time ${t}`);
+            });
+            ambientLightSensor.start();
+        } catch (error) {
+            // Handle construction errors.
+            if (error.name === "SecurityError") {
+                // See the note above about permissions policy.
+                log("Ambient light sensor construction was blocked by a permissions policy.");
+            } else if (error.name === "ReferenceError") {
+                log("Ambient light sensor is not supported by the User Agent.");
+            } else {
+                throw error;
+            }
+        }
+    }
 
-    ambientLightSensor = new AmbientLightSensor();
-    ambientLightSensor.addEventListener("error", (error) => {
-        log(`Ambient light sensor error: ${error.name}`);
-    });
-    ambientLightSensor.addEventListener("activate", () => {
-        log("Ambient light ready to measure");
-    });
-    ambientLightSensor.addEventListener("reading", () => {
-        log(`Current light level: ${ambientLightSensor.illuminance}`);
-    });
-    ambientLightSensor.start();
+    if (window.Gyroscope) {
+        try {
+            gyroscopeSensor = new Gyroscope();
+            gyroscopeSensor.addEventListener("error", (error) => {
+                log(`Gyroscope sensor error: ${error.name}`);
+            });
+            gyroscopeSensor.addEventListener("activate", () => {
+                log("Gyroscope ready to measure");
+            });
+            gyroscopeSensor.addEventListener("reading", () => {
+                const t = gyroscopeSensor.timestamp;
+                log(`Angular velocity along the X-axis ${gyroscopeSensor.x}, time ${t}`);
+                log(`Angular velocity along the Y-axis ${gyroscopeSensor.y}, time ${t}`);
+                log(`Angular velocity along the Z-axis ${gyroscopeSensor.z}, time ${t}`);
+            });
+            gyroscopeSensor.start();
+        } catch (error) {
+            // Handle construction errors.
+            if (error.name === "SecurityError") {
+                // See the note above about permissions policy.
+                log("Gyroscope sensor construction was blocked by a permissions policy.");
+            } else if (error.name === "ReferenceError") {
+                log("Gyroscope sensor is not supported by the User Agent.");
+            } else {
+                throw error;
+            }
+        }
+    }
+
+    if (window.AbsoluteOrientationSensor) {
+        try {
+            orientationSensor = new AbsoluteOrientationSensor();
+            orientationSensor.addEventListener("error", (error) => {
+                log(`Orientation sensor error: ${error.name}`);
+            });
+            orientationSensor.addEventListener("activate", () => {
+                log("Orientation ready to measure");
+            });
+            orientationSensor.addEventListener("reading", () => {
+                const t = orientationSensor.timestamp;
+                const quaternion = orientationSensor.quaternion; // x, y, z, w array
+                log(`Quaternion X element ${quaternion[0]}, time ${t}`);
+                log(`Quaternion Y element ${quaternion[1]}, time ${t}`);
+                log(`Quaternion Z element ${quaternion[2]}, time ${t}`);
+                log(`Quaternion W element ${quaternion[3]}, time ${t}`);
+
+            });
+            orientationSensor.start();
+        } catch (error) {
+            // Handle construction errors.
+            if (error.name === "SecurityError") {
+                // See the note above about permissions policy.
+                log("Gyroscope sensor construction was blocked by a permissions policy.");
+            } else if (error.name === "ReferenceError") {
+                log("Gyroscope sensor is not supported by the User Agent.");
+            } else {
+                throw error;
+            }
+        }
+    }
 }
 
 document.getElementById("startButton").addEventListener(
@@ -96,7 +186,6 @@ document.getElementById("startButton").addEventListener(
     () => {
         startSensors();
     },
-    false,
 );
 
 document.getElementById("stopButton").addEventListener(
@@ -106,10 +195,6 @@ document.getElementById("stopButton").addEventListener(
             accelerometerSensor.stop();
             log("Accelerometer stopped");
         }
-        if (gravitySensor) {
-            gravitySensor.stop();
-            log("Gravity stopped");
-        }
         if (linearAccelerationSensor) {
             linearAccelerationSensor.stop();
             log("Linear acceleration stopped");
@@ -118,8 +203,22 @@ document.getElementById("stopButton").addEventListener(
             ambientLightSensor.stop();
             log("Ambient light stopped");
         }
+        if (gyroscopeSensor) {
+            gyroscopeSensor.stop();
+            log("Gyroscope stopped");
+        }
+        if (orientationSensor) {
+            orientationSensor.stop();
+            log("Orientation stopped");
+        }
     },
-    false,
+);
+
+document.getElementById("clearLogsButton").addEventListener(
+    "click",
+    () => {
+        logElement.value = "";
+    },
 );
 
 // Check permissions and populate html list
